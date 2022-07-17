4a17a06828289ef253c1b6c876bf83ddc236bd56
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-07-17 13:34:45 +0300

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,38 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+
+    private int status;
+
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(true, 100, "hello");
+        error1.printInfo();
+        Error error2 = new Error(true, 55, "buy");
+        error2.printInfo();
+        Error error3 = new Error(false, 100000, "today");
+        error3.printInfo();
+    }
+
+
+}
