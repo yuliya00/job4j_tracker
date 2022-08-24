b234209bbcf1a171b68016ba8dfddc5841f18615
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-08-24 18:27:18 +0300

    Tracker.delete

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -26,13 +26,13 @@ public class Tracker {
 
     public boolean delete(int id) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean rsl = index != -1;
+        if (rsl) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            return true;
         }
-        return false;
+        return rsl;
     }
 
     private int indexOf(int id) {
