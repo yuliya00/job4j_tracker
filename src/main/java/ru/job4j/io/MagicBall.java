43f1c16ea1ae1c984d53aa0e38a5439a6fea98d3
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-08-28 20:08:31 +0300

    magic

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -11,11 +11,9 @@ public class MagicBall {
         int answer = new Random().nextInt(3);
         if (answer == 0) {
             System.out.println("Да.");
-        }
-        if (answer == 1) {
+        } else if (answer == 1) {
             System.out.println("Нет.");
-        }
-        if (answer == 2) {
+        } else {
             System.out.println("Может быть.");
         }
     }
