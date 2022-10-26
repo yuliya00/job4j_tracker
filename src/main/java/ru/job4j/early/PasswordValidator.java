1b76578aa09b1f46da9268e286f91e1e800bb6d9
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-10-26 18:14:02 +0300

    Принципы раннего возврата и охранных выражений [#504875 #354371]

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,49 @@
+package ru.job4j.early;
+
+import java.lang.Character;
+
+public class PasswordValidator {
+    public static String validate(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password can't be null");
+        }
+        if (!(password.length() > 8 && password.length() < 32)) {
+            throw new IllegalArgumentException("Password should be length [8, 32]");
+        }
+        int upperCase = 0;
+        int lowerCase = 0;
+        int digit = 0;
+        int notLetterOrDigit = 0;
+        for (int i = 0; i < password.length(); i++) {
+            char x = password.charAt(i);
+            if (Character.isUpperCase(x)) {
+                upperCase++;
+            }
+            if (Character.isLowerCase(x)) {
+                lowerCase++;
+            }
+            if (Character.isDigit(x)) {
+                digit++;
+            }
+            if (!Character.isLetterOrDigit(x)) {
+                notLetterOrDigit++;
+            }
+        }
+        if (upperCase == 0) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (lowerCase == 0) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (digit == 0) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (notLetterOrDigit == 0) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
+        }
+        if (!password.toLowerCase().contains("qwerty, 12345, password, admin, user")) {
+            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+        }
+        return password;
+    }
+}
