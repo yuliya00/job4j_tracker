36dea88638901332ba43e0b7cf8ba97044848e5d
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2023-01-17 18:21:28 +0300

    Reduce

--- src/main/java/ru/job4j/max/Reduce.java
+++ src/main/java/ru/job4j/max/Reduce.java
@@ -3,8 +3,8 @@ package ru.job4j.max;
 public class Reduce {
     private int[] array;
 
-    public void to(int[] rsl) {
-        array = rsl;
+    public void to(int[] array) {
+        this.array = array;
     }
 
     public void print() {
