7e4f1cfd2aa89e2bce753f4c77fef2063e5d12b6
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2023-01-24 20:13:28 +0300

    Аргументы переменной длины (varargs)

---
+++ src/main/java/ru/job4j/varargs/VarArgs.java
@@ -0,0 +1,28 @@
+package ru.job4j.varargs;
+
+public class VarArgs {
+    public static String text(String... strings) {
+        StringBuilder builder = new StringBuilder();
+        for (String s : strings) {
+            builder.append(s);
+        }
+        return builder + " Количество соединенных элементов: " + strings.length;
+    }
+
+    public static String text(int x, String... strings) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(x);
+        for (String s : strings) {
+            builder.append(s);
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(text());
+        System.out.println(text("aaa"));
+        System.out.println(text("aaa", "bbb", "ccc"));
+        System.out.println(text("aaa", "bbb", "ccc", "ddd", "eee"));
+        System.out.println(text(30, "aaa", "bbb"));
+    }
+}
