c48c3ab1141b22e9e747a34034be6f1253cc067b
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2023-03-25 10:21:24 +0300

    2. Комбинированный компаратор [#10036]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenComparatorByNameAndAsc() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new SortByNameJob());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorByPriorityAndAsc() {
+        Comparator<Job> cmpNamePriority = new JobDescByPriority().thenComparing(new SortByNameJob());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isGreaterThan(0);
+    }
+
+    @Test
+    public void whenComparatorByNameAndDesc() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new SortDescByNameJob());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    public void whenComparatorByPriorityAndDesc() {
+        Comparator<Job> cmpNamePriority = new JobDescByPriority().thenComparing(new SortDescByNameJob());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl).isGreaterThan(0);
+    }
+
+}
