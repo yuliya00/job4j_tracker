ee18a321c6e4802176246895b4d4dc057230d9b9
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2023-03-28 18:20:02 +0300

    3. Компаратор для строк [#35008]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        for (int i = 0; i < Math.min(o1.length(), o2.length()); i++) {
+            int result = Character.compare(o1.charAt(i), o2.charAt(i));
+            if (result != 0) {
+                return result;
+            }
+        }
+        return Integer.compare(o1.length(), o2.length());
+    }
+}
+
